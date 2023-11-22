3e36b5514eefcbf671341ca2297b285cd2eb771c
KiraRak <up.2002@outlook.com>
2023-11-22 18:37:08 -0500

    Atributos del objeto Foods

--- src/main/java/com/mycompany/proyectofinal_poo/Clases/Foods.java
+++ src/main/java/com/mycompany/proyectofinal_poo/Clases/Foods.java
@@ -4,15 +4,52 @@ package com.mycompany.proyectofinal_poo.Clases;
 public class Foods {
     protected String nombre;
     protected String tipo;
+    protected String forma;
+    protected Integer peso;
+    protected String tamano;
+    protected String textura;
+    protected String calorias;
+    protected int proteinas;
+    protected String color;
     
     public Foods(String nombre, String tipo){
         this.nombre = nombre;
         this.tipo = tipo;
     }
     
-    public void showInfo(){
-        System.out.println("Nombre: " + nombre);
-        System.out.println("Tipo: " + tipo);
+    public String getName(){
+        return nombre;
     }
     
+    public String getTipo(){
+        return tipo;
+    }
+    
+    public String getForma(){
+        return forma;
+    }
+    
+    public String getTamano(){
+        return tamano;
+    }
+    
+    public String getTextura(){
+         return textura;
+    }
+    
+    public String Calorias(){
+        return calorias;
+    }
+    
+    public int proteinas(){
+        return proteinas;
+    }
+    
+    public Integer peso(){
+        return peso;
+    }
+    
+    public String color(){
+        return color;
+    }
 }
