50084c396be10fe1f95d3adbc6d325a225ea46bb
L30N4RD087 <106047054+L30N4RD087@users.noreply.github.com>
2023-11-21 21:38:07 -0500

    Add files via upload

---
+++ src/main/java/com/mycompany/proyectofinal_poo/Alimentos.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Leonardo.colmenares
+ */
+public class Alimentos {
+    String nombre;
+    
+
+    public Alimentos(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+}
